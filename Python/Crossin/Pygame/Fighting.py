70f364552d6518b45d8f5294ddf641cdeeb8836e
CamphortreeYH <49608399+CamphortreeYH@users.noreply.github.com>
2019-04-19 17:52:58 +0800

    Add hit, crash

--- Python/Crossin/Pygame/Fighting.py
+++ Python/Crossin/Pygame/Fighting.py
@@ -1,11 +1,12 @@
 import pygame, sys
+from random import *
 pygame.init()
 size = width, height = 450, 800
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Hello World!")
 background = pygame.image.load("back.jpg")
 screen.blit(background, [0, 0])
-# clock = pygame.time.Clock()
+clock = pygame.time.Clock()
 
 class Plane(pygame.sprite.Sprite):
     def __init__(self, image_file, location):
@@ -36,12 +37,26 @@ class Enemy(pygame.sprite.Sprite):
     def move(self):
         self.rect = self.rect.move(self.speed)
         if self.rect.bottom > height:
-            self.speed[1] = 0
+            self.rect.bottom = 0
 
 my_plane = Plane('plane.png', [10, 10])
-my_bullet = Bullet('bullet.png', [0, -5], [10, 10])
-my_enemy = Enemy('enemy.png', [0, 3], [200, -50])
+planeGroup = pygame.sprite.Group(my_plane)
+enemise = []
+for i in range(5):
+    location = [20*(randint(2, 20)), randint(-50, 100)]
+    speed = [0, randint(1, 3)]
+    my_enemy = Enemy('enemy.png', speed, location)
+    enemise.append(my_enemy)
+
+
+bullets = []
+for i in range(5):
+    my_bullet = Bullet('bullet.png', [0, -5], [10, 10])
+    bullets.append(my_bullet)
+bulletGroup = pygame.sprite.Group(my_bullet)
+
 while True:
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
@@ -51,12 +66,17 @@ while True:
             if my_bullet.rect.top <= 0:
                 my_bullet.rect.centery = event.pos[1]
 
-
-    # clock.tick(30)
     screen.blit(background,(0,0))
-    my_bullet.move()
-    screen.blit(my_bullet.image, my_bullet.rect)
+    for bullet in bullets:
+        bullet.move()
+        screen.blit(bullet.image, bullet.rect)
     screen.blit(my_plane.image, my_plane.rect)
-    my_enemy.move()
-    screen.blit(my_enemy.image, my_enemy.rect)
+    for enemy in enemise:
+        enemy.move()
+        if pygame.sprite.spritecollide(enemy, bulletGroup, False):
+            enemy.rect.bottom = 0
+        if pygame.sprite.spritecollide(enemy, planeGroup, False):
+            sys.exit()
+        screen.blit(enemy.image, enemy.rect)
+
     pygame.display.flip()
