b4f3e9f5a38e1af5582ea90645f01ecc178e0285
CamphortreeYH <49608399+CamphortreeYH@users.noreply.github.com>
2019-04-22 17:47:09 +0800

    add code.py

---
+++ Crossin/Web/code.py
@@ -0,0 +1,18 @@
+import web
+
+db = web.database(dbn='sqlite', db='MovieSite.db')
+urls = (
+    '/', 'index',
+    '/movie/(\d+)', 'movie',
+)
+
+render = web.template.render('templates/')
+
+class index:
+    def GET(self):
+        movies = db.select('movie')
+        return render.index(movies)
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
