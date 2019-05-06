080793ff635953345cc67dd541c04887cd6d7f84
CamphortreeYH <49608399+CamphortreeYH@users.noreply.github.com>
2019-05-06 17:31:38 +0800

    Add unit test and doc test example

---
+++ LiaoXuefeng/ErrorDebuggingTesting/Student.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+class Student(object):
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+    def get_grade(self):
+        if self.score >= 80 and self.score <= 100:
+            return 'A'
+        elif self.score >= 60 and self.score < 80:
+            return 'B'
+        elif self.score >= 0 and self.score < 60:
+            return 'C'
+        else:
+            raise ValueError('value is not between 0 and 100')
+
