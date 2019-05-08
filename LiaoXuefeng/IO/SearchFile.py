3c12bebb874ee22450a29fdaa5cd11898bd0df1e
CamphortreeYH <49608399+CamphortreeYH@users.noreply.github.com>
2019-05-08 18:01:53 +0800

    Update SearchFile,py

---
+++ LiaoXuefeng/IO/SearchFile.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+
+import os
+
+def find_file(str,path):
+    for root, dirs, files in os.walk(path, topdown=False):
+        for file in files:
+            if str in file:
+                print(os.path.join(root, file))
+
+find_file("txt", "D:\\Automation\\LiaoXuefeng")
