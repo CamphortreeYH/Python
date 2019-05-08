d2654fddcf56096f691bb9b38d19d26a3e6511b6
CamphortreeYH <49608399+CamphortreeYH@users.noreply.github.com>
2019-05-08 16:27:05 +0800

    Update FileDir.py

--- LiaoXuefeng/IO/FileDir.py
+++ LiaoXuefeng/IO/FileDir.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+#实现 dir -l功能
 from datetime import datetime
 import os
 
@@ -14,3 +15,19 @@ for f in os.listdir(pwd):
     mtime = datetime.fromtimestamp(os.path.getmtime(f)).strftime('%Y-%m-%d %H:%M')
     flag = '/' if os.path.isdir(f) else ''
     print('%10d  %s  %s%s' % (fsize, mtime, f, flag))
+
+#列出MyPath下.py格式的文件信息
+import os.path
+import glob
+from datetime import datetime
+
+pyfiles = glob.glob('*.py')
+MyPath = 'D:\\Automation\\LiaoXuefeng\\IO'
+
+name_sz_date = [(name, os.path.getsize(name), os.path.getatime(name)) for name in pyfiles]
+for name, size, mtime in name_sz_date:
+    print(name, size, datetime.fromtimestamp(mtime).strftime('%Y-%m-%d %H:%M'))
+
+file_metadate = [(name, os.stat(name)) for name in pyfiles]
+for name, meta in file_metadate:
+    print(name, meta.st_size, datetime.fromtimestamp(meta.st_mtime).strftime('%Y-%m-%d %H:%M'))
