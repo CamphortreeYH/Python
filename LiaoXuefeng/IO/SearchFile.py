bbfc47b00b51c57dbf574c00994d2c61bedc6188
CamphortreeYH <49608399+CamphortreeYH@users.noreply.github.com>
2019-05-09 10:52:50 +0800

    Update SearchFile.py

--- LiaoXuefeng/IO/SearchFile.py
+++ LiaoXuefeng/IO/SearchFile.py
@@ -3,7 +3,7 @@
 import os
 
 def find_file(str,path):
-    for root, dirs, files in os.walk(path, topdown=False):
+    for root, dirs, files in os.walk(path):
         for file in files:
             if str in file:
                 print(os.path.join(root, file))
