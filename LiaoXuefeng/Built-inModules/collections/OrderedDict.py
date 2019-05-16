70e0bc048ccb47f8c71bc405ea24ca622629f600
CamphortreeYH <49608399+CamphortreeYH@users.noreply.github.com>
2019-05-16 23:42:14 +0800

    Update OrderedDict.py

--- LiaoXuefeng/Built-inModules/collections/OrderedDict.py
+++ LiaoXuefeng/Built-inModules/collections/OrderedDict.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from collections import OrderedDict
 
 d = dict([('a', 1), ('b', 2), ('c', 3)])
@@ -10,3 +11,20 @@ od1['y'] = 2
 od1['x'] = 3
 list(od1.keys())
 print(od1)
+
+class LastUpdateOrderedDict(OrderedDict):
+    def __init__(self, capacity):
+        super(LastUpdateOrderedDict, self).__init__()
+        self._capacity = capacity
+
+    def __setitem__(self, key, value):
+        scontainsKay = 1 if key in self else 0
+        if len(self) - scontainsKay >= self._capacity:
+            last = self.popitem(last=False)
+            print('remove:', last)
+        if containsKey:
+            del self[key]
+            print('set:', (key, value))
+        else:
+            print('add:', (key, value))
+        OrderedDict.__setitem__(self, key, value)
