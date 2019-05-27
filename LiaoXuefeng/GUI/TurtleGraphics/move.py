b5b973dafec650ab9558da358f6c84b16d85e8ed
CamphortreeYH <49608399+CamphortreeYH@users.noreply.github.com>
2019-05-28 00:00:07 +0800

    Create move.py

---
+++ LiaoXuefeng/GUI/TurtleGraphics/move.py
@@ -0,0 +1,11 @@
+import turtle
+turtle.shape("turtle")
+
+turtle.forward(200) #往前
+turtle.back(300) #往后
+turtle.left(90) #左转90度
+turtle.forward(200) #往前
+turtle.right(90) #右旋转90度
+turtle.forward(200) #往前
+
+turtle.exitonclick()
