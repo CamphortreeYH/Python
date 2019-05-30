630d8a804b2655692c8c17825e962ebb7e036f87
CamphortreeYH <49608399+CamphortreeYH@users.noreply.github.com>
2019-05-30 16:15:50 +0800

    Update withSubject.py

--- LiaoXuefeng/Email/SMTP/withSubject.py
+++ LiaoXuefeng/Email/SMTP/withSubject.py
@@ -22,13 +22,21 @@ password = input('Password:')
 to_addr = input('To:')
 smtp_server = input('SMTP server:')
 
+# 参数1.邮件正文  参数2MIME的subtype,plain表示纯文本，最终的MIME就是text/plain，
+# 最后一定要用utf-8编码保证多语言的兼容性
+# 如果发送HTML邮件 把HTML字符串传进去 把plain变为html
 msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
+# 注意不能简单地传入name <addr@example.com>，因为如果包含中文，需要通过Header对象进行编码。
 msg['From'] = _format_addr('Python爱好者<%s>' % from_addr)
+# msg['To']接收的是字符串而不是list，如果有多个邮件地址，用,分隔即可。
+# 你看到的收件人的名字很可能不是我们传入的管理员，因为很多邮件服务商在显示邮件时，
+# 会把收件人名字自动替换为用户注册的名字，但是其他收件人名字的显示不受影响。
 msg['To'] = _format_addr('管理员<%s>' % to_addr)
 msg['Subject'] = Header('来自SMTP的问候……', 'utf-8').encode()
 
 server = smtplib.SMTP(smtp_server, 25)
 server.set_debuglevel(1)
 server.login(from_addr, password)
+# 发邮件，可以发给多个人，所以是一个list，邮件正文是一个str，as_string()把MIMEText对象变成str
 server.sendmail(from_addr, [to_addr], msg.as_string())
 server.quit()
