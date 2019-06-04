c19a0ef278adc6212616a518fe2c57cb18748100
CamphortreeYH <49608399+CamphortreeYH@users.noreply.github.com>
2019-06-05 00:36:22 +0800

    Add server

---
+++ LiaoXuefeng/WebDevelopping/WSGI/server.py
@@ -0,0 +1,12 @@
+#server.py
+#从wsgiref模块导入:
+from wsgiref.simple_server import make_server
+#导入我们自己辩解的application函数:
+from hello import application
+
+#创建一个服务器，IP地址为空，端口为8000,处理函数是application
+httpd = make_server('', 8000, application)
+print('Server HTTP on port 8000...')
+
+#开始监听HTTP请求:
+httpd.serve_forever()
